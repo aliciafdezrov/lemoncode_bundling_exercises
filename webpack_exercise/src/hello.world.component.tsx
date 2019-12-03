21e775346dfecdbde9358f152ed0e6b1a6e482a6
alicia <aliciafdezrov@gmail.com>
2019-12-03 19:30:44 +0100

    Added comment on hello world component testing html plugin

--- webpack_exercise/src/hello.world.component.tsx
+++ webpack_exercise/src/hello.world.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 const classes = require('./styles/hello.world.component.scss');
+//Aquí hay un comentario en código que en producción no aparecerá gracias a html-webpack-plugin
 
 const getEnvironment = () => process.env.production ? "PRODUCCIÓN" : "DESARROLLO";
 
