41bf1564f2fcb83a9d6b9639b8b79f121631b628
alicia <aliciafdezrov@gmail.com>
2019-12-03 19:18:50 +0100

    Added production configuration, changed style-loader by mini css extract plugin

--- webpack_exercise/pro.webpack.js
+++ webpack_exercise/pro.webpack.js
@@ -5,5 +5,36 @@ module.exports = merge(base, {
     mode: 'production',
     output: {
         filename: '[hash].[name].js',
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                            },
+                            localsConvention: 'camelCase',
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
 });
