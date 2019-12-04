16333f2e1685523a3cd1456bae7e425886e5dc06
alicia <aliciafdezrov@gmail.com>
2019-12-04 16:56:56 +0100

    Found a temporal solution with a problem with typescript (Errno: TS2307)

--- parcel_version/src/index.tsx
+++ parcel_version/src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {HelloWorldComponent} from "./hello.world.component";
 import './styles/index.scss';
-//@ts-ignore
 import image from "./content/react-icon.png";
 
 ReactDOM.render(
