b2eee22353af525435b089b4865fd01ba5aa32ea
alicia <aliciafdezrov@gmail.com>
2019-12-04 16:15:08 +0100

    Added first version of parcel version with React and Typescript but without sass and  image

---
+++ parcel_version/src/hello.world.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles/hello.world.component.scss';
+
+export const HelloWorldComponent = () => {
+    return (
+        <div>
+            <h1>HELLO WORLD FROM REACT!</h1>
+        </div>
+    );
+};
---
+++ parcel_version/src/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {HelloWorldComponent} from "./hello.world.component";
+import './styles/index.scss';
+
+ReactDOM.render(
+        <HelloWorldComponent/>,
+    document.getElementById('react')
+);
