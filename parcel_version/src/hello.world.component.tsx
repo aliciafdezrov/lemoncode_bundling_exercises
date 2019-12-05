95975c66bd2c2a5175a016dc0717c28c1d5b5817
alicia <aliciafdezrov@gmail.com>
2019-12-05 12:37:31 +0100

    Added environment variable to parcel version of the project

--- parcel_version/src/hello.world.component.tsx
+++ parcel_version/src/hello.world.component.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './styles/hello.world.component.scss';
 
+//@ts-ignore
+const getEnvironmentVariableInUpperCase = () => process.env.ENVIRONMENT.toUpperCase();
+
 export const HelloWorldComponent = () => {
     return (
         <div className="flex-container-component">
             <h1>HELLO WORLD FROM REACT!</h1>
+            <h3>THIS IS {getEnvironmentVariableInUpperCase()}</h3>
         </div>
     );
 };
